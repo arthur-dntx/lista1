ce37ae6c4448a20aa90d51ebccbe903142cbfd35
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-15 20:18:27 -0300

    Add files via upload

---
+++ lista1ex2/lista1ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aresta;
+            int area;
+
+            Console.WriteLine("     -- ÁREA DO QUADRADO --");
+
+            Console.WriteLine("Digite o valor de uma aresta em centímetros (cm): ");
+            aresta = int.Parse(Console.ReadLine());
+
+            area = aresta * aresta;
+            Console.WriteLine("A área do quadrado é: {0}cm²", area);
+            Console.ReadKey();
+        }
+    }
+}
