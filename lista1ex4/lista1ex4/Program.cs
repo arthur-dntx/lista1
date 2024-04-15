54c707f81f6ee7f561b373f0933a8cd74af0275f
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-15 20:56:05 -0300

    Add files via upload

---
+++ lista1ex4/lista1ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+            Console.WriteLine("      -- ÁREA DE UM TRIÂNGULO --");
+
+            Console.WriteLine("Digite a medida da base em centímetros (cm): ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura em centímetros (cm): ");
+            a = int.Parse(Console.ReadLine());
+
+            area = (a * b)/2;
+            Console.WriteLine("A área do triângulo é: {0}cm²", area);
+            Console.ReadKey();
+        }
+    }
+}
