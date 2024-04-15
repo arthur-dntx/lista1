8756ab7a54e79a115e6ebe0d0d98613d911664db
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-15 20:17:45 -0300

    Add files via upload

---
+++ lista1ex1/lista1ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+            Console.WriteLine("      -- ÁREA DE UM RETANGULO --");
+
+            Console.WriteLine("Digite a medida da base em centímetros (cm): ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura em centímetros (cm): ");
+            a = int.Parse(Console.ReadLine());
+
+            area = a * b;
+            Console.WriteLine("A área do retângulo é: {0}cm²", area);
+            Console.ReadKey();
+        }
+    }
+}
