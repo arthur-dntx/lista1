403d53a12bc29f2265cf929672b5b05f83728528
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-22 20:45:04 -0300

    Add files via upload

---
+++ lista1ex8/lista1ex8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+
+            Console.WriteLine("    -- CONVERSÃO DE CELSIUS PARA FAHRENHEIT --");
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine("A temperatura em Fahrenheit é: {0} °F", fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
