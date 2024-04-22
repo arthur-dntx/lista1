338f6825e27cdfbc58b0148adb5cf6f5d7f92f26
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-22 20:46:32 -0300

    Add files via upload

---
+++ lista1ex9/lista1ex9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double diametro;
+           double raio;
+           double area;
+
+            Console.WriteLine("    -- DIAMETRO DO CIRCULO --");
+
+           Console.Write("Digite o diâmetro do círculo: ");
+            diametro = Convert.ToDouble(Console.ReadLine());
+
+            raio = diametro / 2;
+            area = Math.PI * (raio * raio);
+            Console.WriteLine("A área do círculo é: {0} unidades de área", area);
+            Console.ReadKey();
+        }
+    }
+}
