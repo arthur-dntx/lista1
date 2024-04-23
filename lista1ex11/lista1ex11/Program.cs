15a3464dc0c6a58f4e4460d2aca4ebfd6917861e
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-22 21:05:16 -0300

    Add files via upload

---
+++ lista1ex11/lista1ex11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("    -- POTÊNCIA DE 'X' ELEVADO A 'Y'");
+
+            Console.Write("Digite o valor de X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Math.Pow(x, y);
+
+            Console.WriteLine("O resultado de X elevado a Y é: {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
