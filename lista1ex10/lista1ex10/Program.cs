e6f2f4194133af239bdbdb30aa59739cf57b10c9
arthur-dntx <165731579+arthur-dntx@users.noreply.github.com>
2024-04-22 21:04:16 -0300

    Add files via upload

---
+++ lista1ex10/lista1ex10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacaoDolar;
+            double quantidadeDolares;
+            double valorEmReais;
+
+            Console.WriteLine("    -- COTAÇÃO DE DÓLARES PARA REAIS --");
+
+            Console.Write("Digite a cotação do dólar: ");
+            cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a quantidade de dólares: ");
+            quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+            valorEmReais = quantidadeDolares * cotacaoDolar;
+            
+            Console.WriteLine("O valor em Reais é: R$ {0", valorEmReais);
+            
+            Console.ReadKey();
+        }
+    }
+}
